b933257070f6c2806a317f04ecd5627bb168c11d
Ivan Gonzalez <ivangonzalez@gmail.com>
2013-09-22 12:24:21 -0700

    remove one seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.create( [
-	{phone_no:'+14159751399' , token: '12222' ,confirm_code: '123'},
+	{phone_no:'+12536938705' , token: '12222' ,confirm_code: '123'},
 	{phone_no:'+16083386540' , token: '12223' ,confirm_code: '123'},
 	{phone_no:'+13152100209' , token: '12224' ,confirm_code: '123'},
-	{phone_no:'+12536938705' , token: '12224' ,confirm_code: '123'},
+	{phone_no:'+14159751399' , token: '12224' ,confirm_code: '123'},
 
 ])
 
@@ -22,6 +22,5 @@ flight = Flight.create([
 rider = Rider.create([
 	{user_id:1, flight_id:2, final_dest: "255 King Street, San Francisco,CA"},
 	{user_id:2, flight_id:2, final_dest: "94110"},
-	{user_id:3, flight_id:2, final_dest: "94080"},
-	{user_id:4, flight_id:2, final_dest: "Potrero Avenue and Mariposa Street, San Francisco"}
+	{user_id:3, flight_id:2, final_dest: "Potrero Avenue and Mariposa Street, San Francisco"}
 ])
